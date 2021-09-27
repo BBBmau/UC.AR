caf333604bc1f83a2ea8dd1a24d8247d15bb417f
Mau <alvarez.mauriciotm@gmail.com>
2021-09-26 22:56:01 -0700

    Initial commit - Played Around with Modifiers in JetPack Compose

---
+++ app/src/main/java/com/mau/ucar/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.mau.ucar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mau.ucar.ui.theme.UCARTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Yellow)
+                    .padding(10.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+            ) {
+                Text("Hello")
+
+                Text("Mauricio!", modifier = Modifier
+                    .border(5.dp, Color.Black)
+                    .padding(5.dp)
+                    .offset(20.dp, 20.dp)
+                    .border(10.dp, Color.Gray)
+                    .padding(10.dp)
+                    .draggable(
+                        orientation = Orientation.Horizontal,
+                        state = rememberDraggableState{}
+                    ))
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("World")
+            }
+
+          //Greeting("Mauricio")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+        Greeting("Android")
+}
